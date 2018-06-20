6e5aa6c7d069ae553d38e4a817795dbdefc0aba3
tangxiumei <tangxiumei@frchina.com>
2018-06-20 18:08:59 +0800

    add symbol

--- dist/decorator.js
+++ dist/decorator.js
@@ -1,14 +1,22 @@
-// function testable (isTestable) {
-//   return function (target) {
-//     target.isTestable = isTestable
-//   }
-// }
-// @testable(true)
-// class MyTestableClass {
-// }
-// console.info(MyTestableClass.isTestable) // true
+import _classCallCheck from "babel-runtime/helpers/classCallCheck";
 
-// @testable(false)
-// class MyClass {
-// }
-// console.info(MyClass.isTestable) // false
+var _dec, _class, _dec2, _class2;
+
+function testable(isTestable) {
+  return function (target) {
+    target.isTestable = isTestable;
+  };
+}
+var MyTestableClass = (_dec = testable(true), _dec(_class = function MyTestableClass() {
+  _classCallCheck(this, MyTestableClass);
+}) || _class);
+
+console.info(MyTestableClass.isTestable); // true
+
+var MyClass = (_dec2 = testable(false), _dec2(_class2 = function MyClass() {
+  _classCallCheck(this, MyClass);
+}) || _class2);
+
+console.info(MyClass.isTestable); // false
+
+export default testable;
