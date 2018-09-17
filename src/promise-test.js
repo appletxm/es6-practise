f36452d75813a3f5ed61feae604eceba9ebcb8e4
appletxm <appletxm@163.com>
2018-09-17 08:32:31 +0800

    add new promise test case

---
+++ src/promise-test.js
@@ -0,0 +1,42 @@
+function letBtoGetDelivery () {
+  console.info('让B去前台拿快递')
+  return new Promise((resove) => {
+    setTimeout(() => {
+      getDelievery()
+      resove(true)
+    }, 2000)
+  })
+}
+
+function getDelievery () {
+  console.info('B去前台拿到快递')
+}
+function putDeliveryToDeskC () {
+  console.info('把快递放到C的桌面，并留纸条让C作检查')
+}
+function letCcheckDelivery () {
+  console.info('C开始检查物品')
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      checkDelivery()
+      resolve(true)
+    }, 5000)
+  })
+}
+
+function checkDelivery () {
+  console.info('C检查快递')
+}
+
+function putDeliveryToBoss () {
+  console.info('再把物品拿给Boss')
+}
+
+letBtoGetDelivery().then(() => {
+  putDeliveryToDeskC()
+  return letCcheckDelivery()
+}).then(() => {
+  putDeliveryToBoss()
+})
+
+export default {}
