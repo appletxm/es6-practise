25775f6395acc9ff8a52858bc919c9c11bfb40e6
tangxiumei <tangxiumei@rfchina.com>
2019-04-15 10:08:28 +0800

    add promise

--- dist/promise-3.js
+++ dist/promise-3.js
@@ -5,12 +5,21 @@ var promise = new _Promise(function (resolve, reject) {
   }, 2000);
 });
 
+var promise2 = new _Promise(function (resolve, reject) {
+  setTimeout(function () {
+    resolve(true);
+  }, 2000);
+});
+
 promise.then(function (res) {
   console.info('promise reslove res:', res);
-}, function (res) {
-  console.info('promise reject res:', res);
 }).catch(function (err) {
   console.info('catch catch err:', err);
+  return promise2;
+}).then(function (res) {
+  console.info('*****', res);
+}).then(function (res) {
+  console.info('=====', res);
 }).finally(function (res) {
   console.info('finally res:', res);
 });
--- src/promise-3.js
+++ src/promise-3.js
@@ -4,12 +4,21 @@ let promise = new Promise(function(resolve, reject) {
   }, 2000)
 })
 
+let promise2 = new Promise(function(resolve, reject) {
+  setTimeout(function(){
+    resolve(true)
+  }, 2000)
+})
+
 promise.then(res => {
   console.info('promise reslove res:', res)
-}, res => {
-  console.info('promise reject res:', res)
 }).catch(err => {
   console.info('catch catch err:', err)
+  return promise2
+}).then(res => {
+  console.info('*****', res)
+}).then(res => {
+  console.info('=====', res)
 }).finally(res => {
   console.info('finally res:', res)
 })
